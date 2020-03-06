3334e0309eb1aac99c1af35fafcee40850a1e198
RAW <rwilliams01101@gmail.com>
2020-03-05 20:44:58 -0600

    cleaned up code, formatted

--- Develop/script.js
+++ Develop/script.js
@@ -19,75 +19,72 @@ function generate() {
     location.reload(true);
     generate()
   }
-  // lowercase y/n
 
   var lowercaseAnswer = confirm("Should the password contain lowercase letters? OK is yes, Cancel is no.");
   if (lowercaseAnswer) {
-    for (var i = 1; i <= length; i++) {
-      password += lowercase.charAt(Math.floor(Math.random() * Math.floor(lowercase.length - 1)));
-      console.log("character", password)
-    }
     passwordDivisor++
     console.log("Include lowercase");
   }
   else {
     console.log("Do not include lowercase");
-    console.log("PDworking", passwordDivisor)
   }
-  // uppercase y/n
-
   var uppercaseAnswer = confirm("Should the password contain uppercase letters? OK is yes, Cancel is no.");
   if (uppercaseAnswer) {
-    for (var i = 1; i <= length; i++) {
-      password += uppercase.charAt(Math.floor(Math.random() * Math.floor(uppercase.length - 1)));
-      console.log("PDworking", passwordDivisor)
-      console.log("character", password)
-    }
     passwordDivisor++
-    console.log("Include uppercase", password);
+    console.log("Include uppercase");
   }
   else {
     console.log("Do not include uppercase");
   }
-  // special y/n
-
   var specialAnswer = confirm("Should the password contain special characters? OK is yes, Cancel is no.");
   if (specialAnswer) {
-    for (var i = 1; i <= length; i++) {
-      password += special.charAt(Math.floor(Math.random() * Math.floor(special.length - 1)));
-      console.log("character", password)
-      console.log("PDworking", passwordDivisor)
-    }
     passwordDivisor++
-    console.log("Include special characters");
+    console.log("Include special");
   }
   else {
-    console.log("Do not include special characters");
+    console.log("Do not include special");
   }
-  // numbers y/n
-
   var numbersAnswer = confirm("Should the password contain numbers? OK is yes, Cancel is no.");
   if (numbersAnswer) {
-    for (var i = 1; i <= length; i++) {
-      password += numbers.charAt(Math.floor(Math.random() * Math.floor(numbers.length - 1)));
-      console.log("character", password)
-      console.log("PDworking", passwordDivisor)
-    }
     passwordDivisor++
     console.log("Include numbers");
-
   }
   else {
     console.log("Do not include numbers");
   }
 
-  if (password != length) {
-    Array.from('password');
-    console.log(Array.from(password));
-    console.log(password.split("").join(0));
-    console.log("Password_Length", password);
+  console.log(length = length / passwordDivisor);
+  console.log(Math.floor(length))
+
+  if (lowercaseAnswer) {
+    for (var i = 1; i <= length; i++) {
+      password += lowercase.charAt(Math.floor(Math.random() * Math.floor(lowercase.length - 1)));
+    }
   }
 
+  if (uppercaseAnswer) {
+    for (var i = 1; i <= length; i++) {
+      password += uppercase.charAt(Math.floor(Math.random() * Math.floor(uppercase.length - 1)));
+    }
+  }
+
+  if (specialAnswer) {
+    for (var i = 1; i <= length; i++) {
+      password += special.charAt(Math.floor(Math.random() * Math.floor(special.length - 1)));
+    }
+  }
+
+  if (numbersAnswer) {
+    for (var i = 1; i <= length; i++) {
+      password += numbers.charAt(Math.floor(Math.random() * Math.floor(numbers.length - 1)));
+    }
+  }
+
+  // Checks 
+  if (password != length) {
+    password = (password + "(")
+  }
+  // if no parameters are assigned by prompts, process will start over 
   else {
     alert("Of all the idiots in all the idiot villages of all the idiot worlds, you stand alone, my friend. - Michael Scott")
     location.reload(true);
@@ -98,7 +95,7 @@ function generate() {
   document.getElementById("password").value = password;
 }
 
-// // Add event listener to generate button
+// event listener to generate button
 var startButton = document.getElementById("redButton")
 startButton.addEventListener("click", function () {
   generate()
